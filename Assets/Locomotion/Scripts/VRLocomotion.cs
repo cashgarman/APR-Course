a538977ce3ce205d70a7b10babacf3f4c7b657e4
Cash Garman <cashgarman@hotmail.co.uk>
2021-06-03 20:31:49 -0700

    Lesson 12

--- Assets/Locomotion/Scripts/VRLocomotion.cs
+++ Assets/Locomotion/Scripts/VRLocomotion.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public enum Handedness
 {
@@ -19,11 +20,21 @@ public class VRLocomotion : MonoBehaviour
     public float teleportRange;
     public Color invalidTeleportColour;
     public Color validTeleportColour;
+    public float smoothAmount;
+    public float arcHeight;
+    public int numLinePoints = 10;
+    public RawImage fadeImage;
+
+    private void Start()
+    {
+        // Make sure we have space for all the points in our teleport beam
+        teleportBeam.positionCount = numLinePoints;
+    }
 
     void Update()
     {
         HandleTeleporting();
-        HandleMovement();
+        //HandleMovement();
         HandleRotation();
     }
 
@@ -36,12 +47,46 @@ public class VRLocomotion : MonoBehaviour
             teleportBeam.enabled = true;
             teleportReticule.gameObject.SetActive(true);
 
+            // Smooth out the movement of the reticule and beam
+            Vector3 startPoint = teleportReticule.transform.position;
+            Vector3 desiredPosition = hit.point;
+            Vector3 deltaToDesiredPosition = desiredPosition - startPoint;
+            Vector3 smoothedDelta = (deltaToDesiredPosition / smoothAmount) * Time.deltaTime;
+            Vector3 newPosition = startPoint + smoothedDelta;
+
+            // Shorter version of the above
+            //newPosition = Vector3.Lerp(startPoint, hit.point, 1f / smoothAmount * Time.deltaTime);
+
             // Move the reticule to the hit position
-            teleportReticule.transform.position = hit.point;
+            teleportReticule.transform.position = newPosition;
+
+            // Calculate the control point of the arc we're trying to create
+            // This will be above the midpoint between the hand and the teleport target
+            Vector3 delta = desiredPosition - startPoint;
+            Vector3 midpoint = startPoint + delta / 2f;
+            Vector3 controlPoint = midpoint + Vector3.up * arcHeight;
+
+            // Shorter version of the above
+            //controlPoint = startPoint + (desiredPosition - startPoint) / 2f + Vector3.up * arcHeight;
+
+            // Calculate all the positions on the curved line
+            for(int i = 0; i < numLinePoints; ++i)
+            {
+                // Calculate percentage
+                float t = (float)i / (numLinePoints - 1);
+
+                // Calculate the 2 component lerps
+                Vector3 startToMiddlePoint = Vector3.Lerp(transform.position, controlPoint, t);
+                Vector3 middleToEndPoint = Vector3.Lerp(controlPoint, newPosition, t);
+                Vector3 positionOnCurve = Vector3.Lerp(startToMiddlePoint, middleToEndPoint, t);
+
+                // Set the point on the line renderer
+                teleportBeam.SetPosition(i, positionOnCurve);
+            }
 
             // Set the start and end positions of the teleport beam
-            teleportBeam.SetPosition(0, transform.position);
-            teleportBeam.SetPosition(1, hit.point);
+            //teleportBeam.SetPosition(0, transform.position);
+            //teleportBeam.SetPosition(1, newPosition);
 
             // Check if the object we hit is an invalid teleport target
             if(hit.transform.gameObject.layer == LayerMask.NameToLayer("InvalidTeleport"))
@@ -60,7 +105,7 @@ public class VRLocomotion : MonoBehaviour
                 if (Input.GetButtonDown($"XRI_{handedness}_TriggerButton"))
                 {
                     // Teleport the player
-                    player.position = hit.point;
+                    StartCoroutine(TeleportPlayer(hit.point));
                 }
             }
         }
@@ -72,51 +117,56 @@ public class VRLocomotion : MonoBehaviour
         }
     }
 
+    private IEnumerator TeleportPlayer(Vector3 point)
+    {
+        // Fade to black
+        /*for(int i = 0; i < 30; ++i)
+        {
+            float t = (float)i / (30 - 1);
+            fadeImage.color = Color.Lerp(Color.clear, Color.black, t);
+
+            yield return new WaitForEndOfFrame();
+        }*/
+
+        float currentTime = 0f;
+        while(currentTime < 1f)
+        {
+            fadeImage.color = Color.Lerp(Color.clear, Color.black, currentTime);
+
+            yield return new WaitForEndOfFrame();
+
+            currentTime += Time.deltaTime;
+        }
+
+        // Changes the position other player
+        player.position = point;
+
+        // Fade back
+        currentTime = 0f;
+        while (currentTime < 1f)
+        {
+            fadeImage.color = Color.Lerp(Color.black, Color.clear, currentTime);
+
+            yield return new WaitForEndOfFrame();
+
+            currentTime += Time.deltaTime;
+        }
+
+        // Fade back
+        /*for(int i = 0; i < 30; ++i)
+        {
+            float t = (float)i / (30 - 1);
+            fadeImage.color = Color.Lerp(Color.black, Color.clear, t);
+
+            yield return new WaitForEndOfFrame();
+        }*/
+    }
+
     private void HandleMovement()
     {
         // Get the forward and strafe inputs
         var forwardInput = Input.GetAxis($"XRI_{handedness}_Primary2DAxis_Vertical");
         var strafeInput = Input.GetAxis($"XRI_{handedness}_Primary2DAxis_Horizontal");
-
-        /*// Step 1:
-
-        // Get the forward and strafe directions
-        var forwardDir = player.forward;
-        var strafeDir = player.right;
-
-        // Change the player's position
-        player.position +=
-            -forwardDir *   // Direction
-            forwardInput *  // Input
-            movementSpeed * // Speed
-            Time.deltaTime; // Frame-rate independence
-
-        player.position +=
-            strafeDir *     // Direction
-            strafeInput *   // Input
-            movementSpeed * // Speed
-            Time.deltaTime; // Frame-rate independence*/
-
-        /*// Step 2
-
-        // Get the forward and strafe directions
-        var forwardDir = Camera.main.transform.forward;
-        var strafeDir = Camera.main.transform.right;
-
-        // Change the player's position
-        player.position +=
-            -forwardDir *   // Direction
-            forwardInput *  // Input
-            movementSpeed * // Speed
-            Time.deltaTime; // Frame-rate independence
-
-        player.position +=
-            strafeDir *     // Direction
-            strafeInput *   // Input
-            movementSpeed * // Speed
-            Time.deltaTime; // Frame-rate independence*/
-
-        // Step 3
 
         // Get the forward and strafe directions
         var forwardDir = Camera.main.transform.forward;
@@ -124,7 +174,7 @@ public class VRLocomotion : MonoBehaviour
         var strafeDir = Camera.main.transform.right;
         strafeDir.y = 0;
 
-        // Step 4
+        // Normalize the directions
         forwardDir.Normalize();
         strafeDir.Normalize();
 
@@ -132,7 +182,7 @@ public class VRLocomotion : MonoBehaviour
         player.position +=
             -forwardDir *   // Direction
             forwardInput *  // Input
-            movementSpeed * // Speed 
+            movementSpeed * // Speed
             Time.deltaTime; // Frame-rate independence
 
         player.position +=
